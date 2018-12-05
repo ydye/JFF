4c28ed71bb3781d0a9bfaeaa17502cd94e841dab
yuye@microsoft.com <yuye@microsoft.com>
2018-12-05 19:51:17 +0800

    26 Golang

---
+++ 26_Remove_Duplicates_from_Sorted_Array/Golang/remove_duplicates_from_sorted_array.go
@@ -0,0 +1,17 @@
+package Golang
+// When submit the code on leetcode, please remove the line packe Golang.
+
+func removeDuplicates(nums []int) int {
+	length := len(nums)
+	if length == 0 {
+		return 0
+	}
+	ret := 1
+	for j := 1; j < length; j++ {
+		if nums[j] != nums[j-1] {
+			nums[ret] = nums[j]
+			ret = ret + 1
+		}
+	}
+	return ret
+}
