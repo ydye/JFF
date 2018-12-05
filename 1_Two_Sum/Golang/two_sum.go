236bb660f2b67a4b2f215aa6c2c3ef35dc077a2d
yuye@microsoft.com <yuye@microsoft.com>
2018-12-05 15:36:38 +0800

    Check in Leetcode 1 golang code“

---
+++ 1_Two_Sum/Golang/two_sum.go
@@ -0,0 +1,31 @@
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+
+func twoSum(nums []int, target int) []int {
+	hashTable := make(map[int]int)
+
+	for idx := range nums {
+		hashTable[nums[idx]] = idx
+	}
+
+	for idx, value := range nums {
+		tmpValue := target - value
+		_, ok := hashTable[tmpValue]
+		if ok != false && idx != hashTable[tmpValue] {
+			return []int{min(idx, hashTable[tmpValue]), max(idx, hashTable[tmpValue])}
+		}
+	}
+	return nil
+}
