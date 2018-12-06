3d2266768157ebedb00ea138f339c40814a19687
yuye@microsoft.com <yuye@microsoft.com>
2018-12-06 19:03:55 +0800

    golang 100

---
+++ 100_Same_Tree/Golang/same_tree.go
@@ -0,0 +1,19 @@
+package Golang
+
+
+// Definition for a binary tree node.
+type TreeNode struct {
+	Val int
+	Left *TreeNode
+	Right *TreeNode
+}
+
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	} else if p == nil || q == nil {
+		return false
+	}
+	return p.Val == q.Val && isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+}
