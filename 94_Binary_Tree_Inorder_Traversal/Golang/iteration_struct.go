84f28d0862bcac8aaad54544151e1b8fcf41c118
ydye <yuye@microsoft.com>
2018-12-20 14:17:34 +0800

    add

--- 94_Binary_Tree_Inorder_Traversal/Golang/iteration_struct.go
+++ 94_Binary_Tree_Inorder_Traversal/Golang/iteration_struct.go
@@ -10,22 +10,29 @@ type TreeNode struct {
 }
 
 type Stack struct {
-	items []*TreeNode
+	Items []*TreeNode
 }
 
 func (s *Stack) New() *Stack {
-	s.items = []*TreeNode{}
+	s.Items = []*TreeNode{}
 	return s
 }
 
 func (s *Stack) Push(t *TreeNode) {
-	s.items = append(s.items, t)
+	s.Items = append(s.Items, t)
 }
 
 func (s *Stack) Pop() *TreeNode {
-	item := s.items[len(s.items)-1]
-	s.items = s.items[0:len(s.items)-1]
+	item := s.Items[len(s.Items)-1]
+	s.Items = s.Items[0:len(s.Items)-1]
 	return item
+}
+
+func (s *Stack) Empty() bool {
+	if len(s.Items) == 0 {
+		return true
+	}
+	return false
 }
 
 
@@ -35,8 +42,23 @@ type Solver struct {
 }
 
 
-func ()
+func (s *Solver) iteration(root *TreeNode) {
+	pCur := root
+	//s.st.New()
+	for pCur != nil || s.st.Empty() == false {
+		if pCur != nil {
+			s.st.Push(pCur)
+			pCur = pCur.Left
+		} else {
+			pCur = s.st.Pop()
+			s.ans = append(s.ans, pCur.Val)
+			pCur = pCur.Right
+		}
+	}
+}
 
 func inorderTraversal(root *TreeNode) []int {
-
+	ret := new(Solver)
+	ret.iteration(root)
+	return ret.ans
 }
